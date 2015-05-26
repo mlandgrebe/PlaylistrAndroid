fc12138bd545705a7bdcf81bce804761e2a0a10b
ztj44 <ztj44@mac.com>
2015-05-25 21:48:20 -0500

    fixed source

--- app/src/main/java/com/attu/attu/MainActivity.java
+++ app/src/main/java/com/attu/attu/MainActivity.java
@@ -15,12 +15,19 @@ import com.spotify.sdk.android.player.Player;
 import com.spotify.sdk.android.player.PlayerNotificationCallback;
 import com.spotify.sdk.android.player.PlayerState;
 
+import java.util.List;
+
 import kaaes.spotify.webapi.android.models.*;
 import kaaes.spotify.webapi.android.*;
 
 import retrofit.client.*;
 import retrofit.RetrofitError;
 import retrofit.Callback;
+import android.widget.*;
+import android.util.*;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.ViewGroup.LayoutParams;
 
 public class MainActivity extends Activity implements
         PlayerNotificationCallback, ConnectionStateCallback {
@@ -35,7 +42,7 @@ public class MainActivity extends Activity implements
     private static final int REQUEST_CODE = 1337;
 
     private Player mPlayer;
-
+    TableLayout country_table;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,10 +50,11 @@ public class MainActivity extends Activity implements
 
         AuthenticationRequest.Builder builder =
                 new AuthenticationRequest.Builder(CLIENT_ID, AuthenticationResponse.Type.TOKEN, REDIRECT_URI);
-        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        builder.setScopes(new String[]{"user-read-private", "streaming", "user-library-read"});
         AuthenticationRequest request = builder.build();
 
         AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+        country_table=(TableLayout)findViewById(R.id.country_table);
     }
 
     @Override
@@ -77,12 +85,13 @@ public class MainActivity extends Activity implements
 // If you know you'll only use the ones that don't require authorisation you can skip this step
                 api.setAccessToken(response.getAccessToken());
 
-                SpotifyService spotify = api.getService();
+                final SpotifyService spotify = api.getService();
 
                 spotify.getAlbum("2dIGnmEIy1WZIcZCFSj6i8", new Callback<Album>() {
                     @Override
                     public void success(Album album, Response response) {
                         Log.d("Album success", album.name);
+
                     }
 
                     @Override
@@ -90,8 +99,72 @@ public class MainActivity extends Activity implements
                         Log.d("Album failure", error.toString());
                     }
                 });
+
+                spotify.getMe(new SpotifyCallback<User>() {
+                    @Override
+                    public void success(User user, Response response) {
+                        // handle successful response
+                        Log.d("Success", user.id);
+                        getUserPlayLists(user.id, spotify);
+                    }
+
+                    @Override
+                    public void failure(SpotifyError error) {
+                        // handle error
+                        Log.d("SavedTracks failure", error.toString());
+                    }
+                });
             }
         }
+    }
+    public void getUserPlayLists(String id, SpotifyService spotify){
+        spotify.getPlaylists(id, new SpotifyCallback<Pager<PlaylistSimple>>() {
+            @Override
+            public void success(Pager<PlaylistSimple> p, Response response) {
+                TableRow row;
+                TextView t1, t2;
+                //Converting to dip unit
+                int dip = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                        (float) 1, getResources().getDisplayMetrics());
+                // handle successful response
+                List<PlaylistSimple> l = p.items;
+                for(PlaylistSimple play: l){
+                    /*
+                    row = new TableRow(this);
+
+                    t1 = new TextView(this);
+                    t2 = new TextView(this);
+
+                    t1.setText(play.name);
+                    t2.setText(play.uri);
+
+                    t1.setTypeface(null, 1);
+                    t2.setTypeface(null, 1);
+
+                    t1.setTextSize(15);
+                    t2.setTextSize(15);
+
+                    t1.setWidth(50 * dip);
+                    t2.setWidth(150 * dip);
+                    t1.setPadding(20*dip, 0, 0, 0);
+                    row.addView(t1);
+                    row.addView(t2);
+
+                    country_table.addView(row, new TableLayout.LayoutParams(
+                            LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+                            */
+                    Log.d("Success", play.name);
+                }
+
+
+            }
+
+            @Override
+            public void failure(SpotifyError error) {
+                // handle error
+                Log.d("SavedTracks failure", error.toString());
+            }
+        });
     }
 
     @Override
