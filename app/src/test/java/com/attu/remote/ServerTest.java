48d485748c51ac2e405591cb11efe817453b6ede
pscollins <pscollins@uchicago.edu>
2015-05-28 17:29:29 -0500

    unit tests

---
+++ app/src/test/java/com/attu/remote/ServerTest.java
@@ -0,0 +1,19 @@
+package com.attu.remote;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created by patrick on 5/28/15.
+ */
+public class ServerTest extends TestCase {
+
+    @Test
+    public void testCreateUser() throws Exception {
+        assertThat(0, equalTo(1));
+
+    }
+}
