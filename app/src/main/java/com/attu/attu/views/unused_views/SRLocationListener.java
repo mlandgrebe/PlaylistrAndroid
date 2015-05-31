4df762c3f36421f6445e1bd5961f46c98f851ca6
mlandgrebe <mlandgrebe@uchicago.edu>
2015-05-31 07:36:45 -0500

    reorganized files and cleaned up code

---
+++ app/src/main/java/com/attu/attu/views/unused_views/SRLocationListener.java
@@ -0,0 +1,40 @@
+/*
+package com.attu.attu.views.unused_views;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import com.attu.models.APIUser;
+
+/**
+ * Created by patrick on 5/29/15.
+ */
+/*
+public class SRLocationListener implements LocationListener {
+    APIUser user;
+
+    public SRLocationListener(APIUser user) {
+        this.user = user;
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        user.setLocation(location);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
+*/
