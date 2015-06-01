79e929d65d07fc3cf5092113765e3c783c7130a9
ztj44 <ztj44@mac.com>
2015-06-01 06:33:55 -0500

    more

---
+++ app/src/main/java/com/attu/attu/views/UpThumb.java
@@ -0,0 +1,27 @@
+package com.attu.attu.views;
+
+/**
+ * Created by zacharyjenkins on 6/1/15.
+ */
+import android.graphics.Color;
+import android.view.View;
+import android.view.View.OnClickListener;
+import com.attu.models.Song;
+import com.attu.models.*;
+import com.attu.util.*;
+
+import kaaes.spotify.webapi.android.models.PlaylistTrack;
+
+public class UpThumb implements OnClickListener {
+    private Song toThumb;
+    public UpThumb(Song song) {
+        toThumb = song;
+    }
+
+    public void onClick(View v) {
+        v.setBackgroundColor(Color.GRAY);
+        State state = State.getState();
+        APIUser user = state.getUser();
+        user.upvote(toThumb);
+    }
+}
